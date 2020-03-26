004cf6db63a9e0309f0a26ad39c9f06eee2ce015
Rajesh Duggal <rajesh@duggalmail.com>
2020-03-26 14:17:45 -0400

    Initial commit

---
+++ src/main/java/com/rajeshduggal/hellospringboot/HomeController.java
@@ -0,0 +1,11 @@
+package com.rajeshduggal.hellospringboot;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Spring Boot!";
+    }
+}
